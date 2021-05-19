834e4f772d8879629ac625ec6cfe37ca77eead03
williamfukushima <wil.fukushima@gmail.com>
2021-05-19 19:03:51 -0300

    Added feature createUser mutation

--- server.ts
+++ server.ts
@@ -1,42 +1,74 @@
 import "reflect-metadata";
+import { createConnection } from "typeorm";
+import { User } from "./src/entity/User";
 
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
 // your data.
 const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
   type Hello {
     hello: String
   }
 
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
   type Query {
-    hello: [Hello]
+    hello: Hello
   }
+
+  type UserType {
+    id: Int!,
+    name: String!,
+    email: String!,
+    password: String!,
+    birthDate: String!
+  }
+
+  type Mutation {
+    createUser(
+        name: String!,
+        email: String!,
+        password: String!,
+        birthDate: String! 
+        ): UserType!
+        
+  }
+
 `;
 const hello = [
-    {
-      hello: 'Hello World!',
-    },
-  ];
-  // Resolvers define the technique for fetching the types defined in the
+  {
+    hello: "Hello World!",
+  },
+];
+// Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
-    Query: {
-      hello: () => hello,
-    },
-  };
-  // The ApolloServer constructor requires two parameters: your schema
+  Query: {
+    hello : () => hello,
+  },
+  Mutation: {
+    createUser: ( _, args ) => createConnection().then(async connection => {
+
+      console.log("Inserting a new user into the database...");
+      const user = new User();
+      user.name = args.name;
+      user.email = args.email;
+      user.password = args.password;
+      user.birthDate = args.birthDate;
+      await connection.manager.save(user);
+      console.log("Saved a new user with id: " + user.id);
+  
+      console.log("Loading users from the database...");
+      const users = await connection.manager.find(User);
+      console.log("Loaded users: ", users);
+      connection.close();
+      return user;
+    })
+  }
+};
+// The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
 const server = new ApolloServer({ typeDefs, resolvers });
-
-
 
 // The `listen` method launches a web server.
 server.listen().then(({ url }: any) => {
