d6cbf0bc7ec96f047896bd7cdb8c0cc9701e7f62
williamfukushima <wil.fukushima@gmail.com>
2021-05-21 10:26:30 -0300

    Fixed to async setup

--- server.ts
+++ server.ts
@@ -48,49 +48,48 @@ const PASSWORD_REGEX = /(?=.*[0-9])(?=.*([A-Za-z])).{7,}/;
 const BIRTHDATE_REGEX =
   /(^(19|20)\d\d)[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])/;
 
-createConnection();
 
-const resolvers = {
-  Query: {
-    hello: () => hello,
-  },
-  Mutation: {
-    createUser: async (_, args) => {
-      try {
-        console.log("Inserting a new user into the database...");
-        const user = new User();
-        user.name = args.name;
-        if (EMAIL_REGEX.test(args.email)) {
-          user.email = args.email;
-        } else throw new ValidationError("E-mail inválido.");
-        if (PASSWORD_REGEX.test(args.password)) {
-          user.password = args.password;
-        } else
-          throw new ValidationError(
-            "Senha deve conter no mínimo 7 caracteres com pelo menos um número e uma letra."
-          );
-        if (BIRTHDATE_REGEX.test(args.birthDate)) {
-          user.birthDate = args.birthDate;
-        } else
-          throw new ValidationError(
-            "Data de Nascimento deve estar no formato yyyy-mm-dd"
-          );
-        await getConnection().manager.save(user);
-        console.log("Saved a new user with id: " + user.id);
-        console.log("Loading users from the database...");
-        const users = await getConnection().manager.find(User);
-        console.log("Loaded users: ", users);
+(async function setup() {
 
-        return user;
-      } catch (err) {
-        return err;
-      }
+  await createConnection();
+
+  const resolvers = {
+    Query: {
+      hello: () => hello,
     },
-  },
-};
+    Mutation: {
+      createUser: async (_, args) => {
+          const user = new User();
+          user.name = args.name;
+          if (EMAIL_REGEX.test(args.email)) {
+            user.email = args.email;
+          } else throw new ValidationError("E-mail inválido.");
+          if (PASSWORD_REGEX.test(args.password)) {
+            user.password = args.password;
+          } else
+            throw new ValidationError(
+              "Senha deve conter no mínimo 7 caracteres com pelo menos um número e uma letra."
+            );
+          if (BIRTHDATE_REGEX.test(args.birthDate)) {
+            user.birthDate = args.birthDate;
+          } else
+            throw new ValidationError(
+              "Data de Nascimento deve estar no formato yyyy-mm-dd"
+            );
+          await getConnection().manager.save(user);
+          console.log("Saved a new user with id: " + user.id);
+          console.log("Loading users from the database...");
+          const users = await getConnection().manager.find(User);
+          console.log("Loaded users: ", users);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+          return user;
+      },
+    },
+  };
 
-server.listen().then(({ url }: any) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+  const server = new ApolloServer({ typeDefs, resolvers });
+
+  server.listen().then(({ url }: any) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
+})();
