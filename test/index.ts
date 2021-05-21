624858c3c20cf80888753173dca6df6ef05fc945
williamfukushima <wil.fukushima@gmail.com>
2021-05-21 18:32:07 -0300

    Added tests setup and scripts.

--- test/index.ts
+++ test/index.ts
@@ -1,36 +1,34 @@
-
 import { setup } from "../server";
 
 const assert = require("assert");
-const request = require('supertest');
+const request = require("supertest");
 const { gql } = require("apollo-server");
 
-const helloQuery = gql`query hello{
-  hello{
-     hello
-    }
-  }`
-console.log(helloQuery);
 
-before(async () =>  {
+before(async () => {
   await setup();
 });
 
-describe('Hello World Query', function() {
-  it('Should respond with Hello World! json object', function (done) {
-    request('localhost:4000').post('/')
-      .send({'query': 
-      `query hello{
+describe("Hello World Query", function () {
+  it("Should respond with Hello World! json object", function (done) {
+    request("localhost:4000")
+      .post("/")
+      .send({
+        query: `query hello{
         hello{
            hello
           }
-        }`})
-      .set('Accept', 'application/json')
-      .expect(200, 
+        }`,
+      })
+      .set("Accept", "application/json")
+      .expect(
+        200,
         {
           data: {
-            hello: {hello :'Hello World!'}
-          }
-        }, done);
+            hello: { hello: "Hello World!" },
+          },
+        },
+        done
+      );
   });
 });
