0629e30c1eff4c4b81560d2ad3a98bbc78759877
williamfukushima <wil.fukushima@gmail.com>
2021-05-24 14:59:45 -0300

    Added createUser mutation test.

--- test/index.ts
+++ test/index.ts
@@ -3,20 +3,30 @@ import * as dotenv from "dotenv";
 dotenv.config({ path: "./test.env" });
 
 import { setup } from "../server";
-
 import * as assert from "assert";
 import * as request from "supertest";
 import { gql } from "apollo-server";
 import * as chai from "chai";
 
+import { getConnection } from "typeorm";
+
 const expect = chai.expect;
 
 before(async () => {
   await setup();
+  await getConnection()
+  .manager
+  .clear("User");
+});
+
+beforeEach(async () => {
+  await getConnection()
+    .manager
+    .clear("User");
 });
 
 describe("Hello World Query", () => {
-  it("Should respond with Hello World! json object", async () => {
+  it("Should respond with Hello World! json object.", async () => {
     const response = await request("localhost:4000")
       .post("/")
       .send({
@@ -32,5 +42,38 @@ describe("Hello World Query", () => {
       .to.have.property("hello")
       .to.have.property("hello")
       .to.equal("Hello World!");
+  });
+});
+
+describe("createUser Mutation", () => {
+  it("Should create a user in the database.", async () => {
+    const response = await request("localhost:4000")
+      .post("/")
+      .send({
+        query: `mutation CreateUser($name: String!, $email: String!, $password: String!, $birthDate: String!){
+        createUser(
+          name: $name,
+          email: $email,
+          password: $password,
+          birthDate: $birthDate)
+        {
+          id
+          name
+          email
+          password
+          birthDate
+        }
+      }`,
+        variables: {
+          name: "a",
+          email: "amdi@asdfasdf.com",
+          password: "1234asfdf",
+          birthDate: "2001-08-03",
+        },
+      });
+    expect(response.body.data.createUser.id).to.be.a("number");
+    expect(response.body.data.createUser.name).to.equal("a");
+    expect(response.body.data.createUser.email).to.equal("amdi@asdfasdf.com");
+    expect(response.body.data.createUser.birthDate).to.equal("2001-08-03");
   });
 });
