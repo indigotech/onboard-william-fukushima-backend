14e4c3d373ee45134c1def481310a96773aa6890
williamfukushima <wil.fukushima@gmail.com>
2021-05-26 14:00:13 -0300

    Fixed issues from login and tests PRs

--- src/mutations/create-user.ts
+++ src/mutations/create-user.ts
@@ -2,35 +2,19 @@ import { getConnection } from "typeorm";
 import { User } from "../entity/User";
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
-import { ValidationError } from "../types-and-classes/errors";
+import { emailValidation, passwordValidation, birthDateValidation } from "../field-validations";
 
 const saltRounds = 10;
-
-const EMAIL_REGEX = /([a-z0-9])+@([a-z0-9])+.com/;
-const PASSWORD_REGEX = /(?=.*[0-9])(?=.*([A-Za-z])).{7,}/;
-const BIRTHDATE_REGEX =
-  /(^(19|20)\d\d)[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])/;
 
 export const createUser = async (_, args, context) => {
 
   jwt.verify(context.authScope, process.env.JWT_SECRET);
   const user = new User();
   user.name = args.name;
-  if (!EMAIL_REGEX.test(args.email)) {
-    throw new ValidationError("E-mail inválido.", 400);
-  }
-  if (!PASSWORD_REGEX.test(args.password)) {
-    throw new ValidationError(
-      "Senha deve conter no mínimo 7 caracteres com pelo menos um número e uma letra.",
-      400
-    );
-  }
-  if (!BIRTHDATE_REGEX.test(args.birthDate)) {
-    throw new ValidationError(
-      "Data de Nascimento deve estar no formato yyyy-mm-dd",
-      400
-    );
-  }
+
+  emailValidation(args.email);
+  passwordValidation(args.password);
+  birthDateValidation(args.birthDate);
   
   user.email = args.email;
   user.salt = await bcrypt.genSaltSync(saltRounds);
--- src/mutations/login.ts
+++ src/mutations/login.ts
@@ -4,22 +4,13 @@ import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { ValidationError, BadCredentials } from "../types-and-classes/errors";
 import { CompleteUserType } from "../types-and-classes/dataTypes";
-
-const EMAIL_REGEX = /([a-z0-9])+@([a-z0-9])+.com/;
-const PASSWORD_REGEX = /(?=.*[0-9])(?=.*([A-Za-z])).{7,}/;
-const BIRTHDATE_REGEX =
-  /(^(19|20)\d\d)[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])/;
+import { emailValidation, passwordValidation } from "../field-validations";
 
 export const login = async (_, args) => {
-  if (!EMAIL_REGEX.test(args.email)) {
-    throw new ValidationError("E-mail inválido.", 400);
-  }
-  if (!PASSWORD_REGEX.test(args.password)) {
-    throw new ValidationError(
-      "Senha deve conter no mínimo 7 caracteres com pelo menos um número e uma letra.",
-      400
-    );
-  }
+
+  emailValidation(args.email);
+  passwordValidation(args.password);
+  
   const user: any = await getConnection().manager.findOne("user", {
     email: args.email,
   });
