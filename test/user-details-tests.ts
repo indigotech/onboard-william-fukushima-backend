4bd614fc3022c9a1f0cdf2fe9e80b7fbebebf52c
williamfukushima <wil.fukushima@gmail.com>
2021-05-27 15:10:12 -0300

    Added more test cases and refactored variables

--- test/user-details-tests.ts
+++ test/user-details-tests.ts
@@ -48,3 +48,20 @@ describe("User details test", () => {
     expect(response.body.data.user.birthDate).to.equal("2000-01-01");
   });
 });
+describe("User details test - fail case. ID not found", () => {
+  it("Should fail to find ID.", async () => {
+    const admin = new User();
+    admin.email = "admin@taqtile.com";
+    admin.name = "admin";
+    admin.birthDate = "2000-01-01";
+    admin.salt = await bcrypt.genSaltSync(10);
+    admin.password = await bcrypt.hashSync(process.env.ADMIN_PASS, admin.salt);
+    await getRepository(User).manager.save(admin);
+    token = await jwt.sign({ id : '1' }, process.env.JWT_SECRET, {
+      expiresIn: "2h",
+    });
+    const response = await userQueryRequest({id: 0});
+    expect(response.body.errors[0].message).to.equal("ID não listado.");
+    expect(response.body.errors[0].httpCode).to.equal(404);
+  });
+});
