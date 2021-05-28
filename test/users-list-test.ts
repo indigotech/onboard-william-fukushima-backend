2cc4bfe42ed1b1e9cd1768686492b655aba99e65
williamfukushima <wil.fukushima@gmail.com>
2021-05-28 13:09:21 -0300

    Added more tests regarding users list

--- test/users-list-test.ts
+++ test/users-list-test.ts
@@ -8,9 +8,20 @@ import * as jwt from "jsonwebtoken";
 import { getRepository } from "typeorm";
 import { userSeeding } from "../src/seeding/seeding";
 import { argsToArgsConfig } from "graphql/type/definition";
+import { userSeeds } from "../src/seeding/seeds";
 
 const expect = chai.expect;
 var token = "";
+
+const getUsers = async (offset, limit) =>
+  await getRepository(User).find({
+    select: ["id", "name", "email", "birthDate"],
+    order: {
+      name: "ASC",
+    },
+    skip: offset,
+    take: limit,
+  });
 
 const usersQueryRequest = async (args) =>
   await request("localhost:4000")
@@ -41,27 +52,31 @@ const usersQueryRequest = async (args) =>
 describe("Users paginated list test", function () {
   it("Should return a list of users for a specified limit and no offset.", async () => {
     await userSeeding();
+    const expectedUsers = await getUsers(0, 10);
     token = await jwt.sign({ id: "1" }, process.env.JWT_SECRET, {
       expiresIn: "2h",
     });
-
-    const response = await usersQueryRequest({ limit: 20 });
+    
+    const response = await usersQueryRequest({});
 
     expect(response.body.data.users.count).to.equal(50);
-    expect(response.body.data.users.users.length).to.equal(20);
+    expect(response.body.data.users.users.length).to.equal(10);
+    expect(response.body.data.users.users).to.have.deep.members(expectedUsers);
     expect(response.body.data.users.hasNextPage).to.equal(true);
     expect(response.body.data.users.hasPreviousPage).to.equal(false);
     expect(response.body.data.users.offset).to.equal(0);
-    expect(response.body.data.users.limit).to.equal(20);
+    expect(response.body.data.users.limit).to.equal(10);
   });
 
   it("Should return a list of users for a specified limit and middle page.", async () => {
     await userSeeding();
+    const expectedUsers = await getUsers(20, 20);
 
     const response = await usersQueryRequest({ limit: 20, offset: 20 });
 
     expect(response.body.data.users.count).to.equal(50);
     expect(response.body.data.users.users.length).to.equal(20);
+    expect(response.body.data.users.users).to.have.deep.members(expectedUsers);
     expect(response.body.data.users.hasNextPage).to.equal(true);
     expect(response.body.data.users.hasPreviousPage).to.equal(true);
     expect(response.body.data.users.offset).to.equal(20);
@@ -70,11 +85,13 @@ describe("Users paginated list test", function () {
 
   it("Should return a list of users for a specified limit and last page.", async () => {
     await userSeeding();
+    const expectedUsers = await getUsers(40, 10);
 
     const response = await usersQueryRequest({ limit: 10, offset: 40 });
 
     expect(response.body.data.users.count).to.equal(50);
     expect(response.body.data.users.users.length).to.equal(10);
+    expect(response.body.data.users.users).to.have.deep.members(expectedUsers);
     expect(response.body.data.users.hasNextPage).to.equal(false);
     expect(response.body.data.users.hasPreviousPage).to.equal(true);
     expect(response.body.data.users.offset).to.equal(40);
@@ -83,11 +100,13 @@ describe("Users paginated list test", function () {
 
   it("Should return a list of users for a specified limit and last page (requesting more than existing number of queries)", async () => {
     await userSeeding();
+    const expectedUsers = await getUsers(40, 20);
 
     const response = await usersQueryRequest({ limit: 20, offset: 40 });
 
     expect(response.body.data.users.count).to.equal(50);
     expect(response.body.data.users.users.length).to.equal(10);
+    expect(response.body.data.users.users).to.have.deep.members(expectedUsers);
     expect(response.body.data.users.hasNextPage).to.equal(false);
     expect(response.body.data.users.hasPreviousPage).to.equal(true);
     expect(response.body.data.users.offset).to.equal(40);
